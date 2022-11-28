7c4ac495e9af7856d173732a99adf5da75e7ad40
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2022-11-28 10:14:56 +0800

    updated ecr image link

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,7 +30,7 @@ resource "aws_apprunner_service" "node-aws-apprunner-service" {
       image_configuration {
         port = "3000"
       }
-      image_identifier      = "504776330223.dkr.ecr.ap-southeast-1.amazonaws.com/node-aws-apprunner-repo:latest"
+      image_identifier      = "504776330223.dkr.ecr.ap-northeast-1.amazonaws.com/node-aws-apprunner-repo:latest"
       image_repository_type = "ECR_PUBLIC"
     }
     auto_deployments_enabled = false
